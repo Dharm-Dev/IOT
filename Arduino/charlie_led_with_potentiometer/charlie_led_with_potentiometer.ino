a4538b5eddb67e16a9480cd3cc7f62341449c7a3
Dharm Vashisth <42669733+Dharm-Dev@users.noreply.github.com>
2020-01-03 18:13:16 +0530

    Update charlie_led_with_potentiometer.ino

--- Arduino/charlie_led_with_potentiometer/charlie_led_with_potentiometer.ino
+++ Arduino/charlie_led_with_potentiometer/charlie_led_with_potentiometer.ino
@@ -1,19 +1,21 @@
-int pm=A6,prev,v;
+/*
+Author: Dharm Vashisth
+Description :Controlling Charlie Multiplexing LEds with 4 pins 
+              without using Loop and glow leds with some delay 
+              controlled by our Potentiometer. 
+*/
+int pm=A6;
 void setup() {
   // put your setup code here, to run once:
   pinMode(pm,INPUT);//potentiometer
-  pinMode(A1,INPUT);//btn
-  Serial.begin(9600);
 }
 int input=0,output=1;
-int val,p=0,f=1;
+int p=0;
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  //reading value from potentiometer.
   val=map(analogRead(pm),0,1023,0,11);
- if(f){
-  switch(val){
-    case 0:
+  
   //1
   pinMode(A5,OUTPUT);//vcc
   pinMode(A4,OUTPUT);//gnd
@@ -22,9 +24,7 @@ void loop() {
   digitalWrite(A5,1);//vcc 1
   digitalWrite(A4,0);//gnd 0
   delay(p);
-    break;
-
-  case 1:
+  
   //2
   pinMode(A5,OUTPUT);//vcc
   pinMode(A2,input);
@@ -32,9 +32,7 @@ void loop() {
   pinMode(A3,output);
   digitalWrite(A3,0);
   delay(p);
-    break;
-
-   case 2:
+  
   //3
   pinMode(A5,OUTPUT);//vcc
   pinMode(A4,input);
@@ -42,9 +40,7 @@ void loop() {
   pinMode(A2,output); //new gnd
   digitalWrite(A2,0);
   delay(p);
-    break;
-    
-    case 3:
+  
   //4
   pinMode(A4,output); //new vcc
   pinMode(A5,input);  //previous vcc
@@ -53,9 +49,7 @@ void loop() {
   digitalWrite(A4,1);
   digitalWrite(A3,0);
   delay(p);
-      break;
-    case 4:
-    
+     
   //5
   pinMode(A4,output);//vcc
   pinMode(A5,input);
@@ -63,36 +57,15 @@ void loop() {
   pinMode(A2,output); //new gnd
   digitalWrite(A2,0);
   delay(p);
-        break;
-
-      case 5:
       
   //6
-  
   pinMode(A4,output);//vcc
   pinMode(A3,input);
   pinMode(A2,input);  //prev gnd
   pinMode(A5,output); //new gnd
   digitalWrite(A5,0);
-//btn code start
-  v=digitalRead(A1);
-  prev=v;
-  Serial.println(v);
-  if(!v){
-      delay(50);
-      v=digitalRead(A1);
-      while(prev==v){
-        prev=v;
-        v=digitalRead(A1);
-      }
-      f=0;
-      Serial.print("You Won");
-    }
-    //end code.
-    delay(p);
-      break;
-
-      case 6:
+  delay(p);
+  
   //7
   pinMode(A3,output); //new vcc
   pinMode(A4,input);  //previous vcc
@@ -101,9 +74,7 @@ void loop() {
   digitalWrite(A3,1);
   digitalWrite(A2,0);
   delay(p);
-        break;
 
-        case 7:
   //8
   pinMode(A3,output);//vcc
   pinMode(A4,input);
@@ -111,20 +82,14 @@ void loop() {
   pinMode(A5,output); //new gnd
   digitalWrite(A5,0);
   delay(p);
-
-    break;
-
-    case 8:
-   //9
+ //9
   pinMode(A3,output);//vcc
   pinMode(A2,input);
   pinMode(A5,input);  //prev gnd
   pinMode(A4,output); //new gnd
   digitalWrite(A4,0);
   delay(p);
-    break;
-
-    case 9:
+  
   //10
   pinMode(A2,output); //new vcc
   pinMode(A3,input);  //previous vcc
@@ -133,9 +98,7 @@ void loop() {
   digitalWrite(A2,1);
   digitalWrite(A5,0);
   delay(p);
-      break;
 
-    case 10:
   //11
   pinMode(A2,output); //vcc
   pinMode(A3,input);
@@ -143,10 +106,7 @@ void loop() {
   pinMode(A4,output); //new gnd
   digitalWrite(A4,0);
   delay(p);
-
-      break;
-     
-     case 11: 
+ 
    //12
     pinMode(A2,output);
     pinMode(A5,input);
@@ -154,15 +114,4 @@ void loop() {
     pinMode(A3,output); //new gnd
     digitalWrite(A3,0);
     delay(p);
-    break;
-    
-  }
- }//end if
- else
- {
-    
-    Serial.println("You Won");
-    delay(3000);
-    f=1;
- }
 }
