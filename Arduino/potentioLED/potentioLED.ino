6c23d4c2184054dc0932781a02394bd0cca26753
Dharm Vashisth <42669733+Dharm-Dev@users.noreply.github.com>
2020-01-03 18:16:07 +0530

    Update potentioLED.ino

--- Arduino/potentioLED/potentioLED.ino
+++ Arduino/potentioLED/potentioLED.ino
@@ -1,20 +1,15 @@
+/*
+  Author:Dharm Vashisth
+  Description: Illustrate PWM on LED controlled by Potentiometer.
+*/
 void setup() {
   // put your setup code here, to run once:
 pinMode(A6,INPUT);
 pinMode(9,OUTPUT);
-pinMode(10,OUTPUT);
-pinMode(11,OUTPUT);
-
-Serial.begin(9600);
 }
 int v;
 void loop() {
-//  v=analogRead(A6);
-//  Serial.println(v);
-//  v=v/4;
-  analogWrite(9,123);
-  analogWrite(10,223);
-  analogWrite(11,253);
-  
+  v=map(analogRead(A6),0,1023,0,255);
+  analogWrite(9,v);
   delay(20);
 }
