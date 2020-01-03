cadbf83378d50a5fdc04e37bfab5ae9ac7126187
Dharm Vashisth <42669733+Dharm-Dev@users.noreply.github.com>
2020-01-03 18:18:04 +0530

    Update rgb_Intensity.ino

--- Arduino/rgb_Intensity/rgb_Intensity.ino
+++ Arduino/rgb_Intensity/rgb_Intensity.ino
@@ -1,3 +1,7 @@
+/*
+    Author: Dharm Vashisth
+    Description: Controlling intensity of RGB LEd's using Potentiometer.
+*/
 int r=9,g=10,b=11,pm=A6;
 int c,c2,c3,val;
 void setup() {
@@ -12,7 +16,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   val=analogRead(pm);
-  c=val/4;
+  c=val/4;  //we can also use map().
   c2=c2?c+30:5;
   c3=c3?c+80:10;
   
