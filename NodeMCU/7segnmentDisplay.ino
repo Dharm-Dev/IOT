d79f4ea115f94b069c7c18c3f4efbaa4f68b904a
Dharm Vashisth <42669733+Dharm-Dev@users.noreply.github.com>
2020-01-03 17:50:11 +0530

    Update 7segnmentDisplay.ino

--- NodeMCU/7segnmentDisplay.ino
+++ NodeMCU/7segnmentDisplay.ino
@@ -1,3 +1,8 @@
+/*
+Author : Dharm Vashisth
+Date : 03/01/20
+Description: Displaying numbers on 7 segnment Common Cathod.
+*/
 #define a 16
 #define b 5
 #define c 4
@@ -6,7 +11,7 @@
 #define f 14
 #define g 12
 #define n 10
-String ar[]={//common annode
+String ar[]={// pattern array
   "1111110",//0
   "0110000",//1
   "1101101",//2
@@ -18,11 +23,10 @@ String ar[]={//common annode
   "1111111",//8
   "1111011"//9
 };
-int tp=7;//total pins
-int p[]={a,b,c,d,e,f,g};
+int tp=7,dl=500;//total pins and delay of 500ms.
+int p[]={a,b,c,d,e,f,g};  //pin numbers
 String tmp; //storing string
 void setup(){
-  Serial.begin(9600);
   for(int i=0;i<tp;i++){
     pinMode(p[i],OUTPUT);
     digitalWrite(p[i],1);    
@@ -30,7 +34,7 @@ void setup(){
 }
 void loop(){
   for(int j=0;j<n;j++){
-    delay(500);
+    delay(dl);
     tmp=ar[j];
     for(int k=0;tmp[k]!='\0';k++){
           if(tmp[k]=='1')
@@ -39,5 +43,4 @@ void loop(){
             digitalWrite(p[k],1); //off
     }
   }
-
 }
