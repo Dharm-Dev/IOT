df92ab01f9244c170db9bbedfeb501fa61bcd1d3
Dharm Vashisth <42669733+Dharm-Dev@users.noreply.github.com>
2020-01-03 17:59:34 +0530

    Update buzzer.ino

--- Arduino/buzzer/buzzer.ino
+++ Arduino/buzzer/buzzer.ino
@@ -1,10 +1,16 @@
-int p=6;
+/*
+Author : Dharm Vashisth
+Description: Implementing Buzzer interface with Arduino Nano
+*/
+int p=6;  //Buzzer is connected to pin number and ground.
 void setup() {
   // put your setup code here, to run once:
-  pinMode(p,OUTPUT);
+  pinMode(p,OUTPUT);  //set pinMode
 }
-
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(p,1);
+  digitalWrite(p,1);//power on the buzzer for 300ms.
+  delay(300);
+  digitalWrite(p,0);  //power off the buzzer and give a delay of 1s.
+  delay(1000);
 }
